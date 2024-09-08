73c8747f779a5ede84ed06649d2c2e9e8219c7a2
ktcv31 <tatiana.carvajal0831@gmail.com>
2024-09-08 10:03:43 -0500

    Create header, footer and navegation

---
+++ TCDigitalportfolio/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <a href="https://github.com/ktcv31?tab=repositories">GitHub</a>
+      <a href="https://www.linkedin.com/in/karen-tatiana-carvajal-4531b6185/">LinkedIn</a>
+    </footer>
+  );
+};
+
+export default Footer;
